7a26f01421c9e5b8a0a44944975374a4053db0e8
SamuCrow <samucrow@gmail.com>
2025-05-13 00:05:55 +0200

    Update get-visitors.js

--- api/get-visitors.js
+++ api/get-visitors.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const siteID = "hackingatlas.github.io";
-    const plausibleApiUrl = `GET https://plausible.io/api/v1/stats/aggregate?site_id=${siteID}&period=30d`;
+    const plausibleApiUrl = `GET https://plausible.io/api/v1/stats/aggregate?site_id=${siteID}&period=30d&metrics=visitors`;
     const apiKey = process.env.PLAUSIBLE_API_KEY;
 
     try {
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
 
         const data = await response.json();
         console.log(data);
-        const visitorsLastMonth = data.results.visitors.value;
+        const visitorsLastMonth = data.visitors;
 
         res.status(200).json({ visitors: visitorsLastMonth });
     } catch (error) {
