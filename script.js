ac53953885982cab2af987d2923546992da34cba
SamuCrow <samucrow@gmail.com>
2025-05-12 13:33:52 +0200

    start hackingatlas web

---
+++ script.js
@@ -0,0 +1,48 @@
+function timeAgo(publishedDate) {
+    const now = new Date();
+    const published = new Date(publishedDate);
+    const difference = now - published;
+    const differenceInHours = Math.floor(difference / (1000 * 60 * 60));
+    const differenceInDays = Math.floor(difference / (1000 * 60 * 60 * 24));
+    
+    if (differenceInDays > 0) {
+        return `${differenceInDays} day${differenceInDays === 1 ? '' : 's'} ago`;
+    }
+    return `${differenceInHours} hour${differenceInHours === 1 ? '' : 's'} ago`;
+}
+
+
+function displayWriteup() {
+    fetch('/writeups/writeups.json')
+        .then(response => response.json())
+        .then(data => {
+            // Get first writeup "[0]"
+            const writeup = data[0];
+            const lastWriteupDiv = document.getElementById('last-writeup');
+
+            lastWriteupDiv.innerHTML = `
+                <div class="last-machine-first-info">
+                    <span class="published text">Published ${timeAgo(writeup.published)}</span>
+                    <div class="difficulty-os">
+                        <span class="difficulty">${writeup.difficulty}</span>
+                        <span class="os text">${writeup.os}</span>
+                    </div>
+                </div>
+                <h2 class="machine-name">${writeup.name}</h2>
+                <p class="machine-description text">${writeup.description}</p>
+                <div class="tags-div">
+                    ${writeup.tags.map(tag => `<span class="tag">#${tag}</span>`).join('\n')}
+                </div>
+                <div class="user-div">
+                    <img class="samucrow-logo" src="/images/samucrow_logo.avif" alt="SamuCrow">
+                    <div class="user-description">
+                        <h3 class="username">SamuCrow</h3>
+                        <span class="user-definition text">Pentester</span>
+                    </div>
+                </div>
+            `;
+        })
+        .catch(error => console.error('Error loading writeups:', error));
+}
+
+document.addEventListener('DOMContentLoaded', displayWriteup);
